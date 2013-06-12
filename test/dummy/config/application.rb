c2392fc54c5bd27488501fa085ee486acfa35498
Jyr Gaxiola <jyr.gaxiola@gmail.com>
2013-06-12 02:26:31 -0500

    fix path

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -3,7 +3,7 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 
 Bundler.require(*Rails.groups)
-require "api_territory"
+require "api/territory"
 
 module Dummy
   class Application < Rails::Application
